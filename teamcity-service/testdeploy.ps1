c7b5fc24e383bc9be7599d42abe646cffc5278a6
Graeme Irwin <graeme_irwin@transcanada.com>
2015-07-05 10:06:39 -0600

    Added teamcity service

---
+++ teamcity-service/testdeploy.ps1
@@ -0,0 +1,18 @@
+
+$testNum=41
+$testName="dotest$($testNum)"
+	
+$location = 'West US'
+New-AzureResourceGroup -Name $testName -DeploymentName "$($testName)deploy" -Location $location `
+	-TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json `
+	-newStorageAccountName "$($testName)storage" `
+	-virtualNetworkName "$($testName)vnt" `
+	-adVMName "$($testName)advm" `
+	-domainName "$($testName).com" `
+	-addnsName "$($testName)-ad" `
+	-locationFromTemplate $location `
+	-DatabaseServerName "$testName" `
+	-DatabaseName "$($testName)DB" `
+	-TeamCityDNSNameForPublicIP "$($testName)-tcs" `
+	-Verbose
+		
