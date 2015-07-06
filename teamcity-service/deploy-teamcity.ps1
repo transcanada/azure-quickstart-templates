eb13cc576dc5242771289fad66f6483c5e11e895
Graeme Irwin <graeme_irwin@transcanada.com>
2015-07-06 12:31:45 -0600

    Ripped out domain  controller stuff

--- teamcity-service/deploy-teamcity.ps1
+++ teamcity-service/deploy-teamcity.ps1
@@ -1,18 +1,14 @@
 
-$testNum=58
-$testName="dotest$($testNum)"
+$testNum=5
+$testName="tctest$($testNum)"
 	
 $location = 'West US'
 New-AzureResourceGroup -Name $testName -DeploymentName "$($testName)deploy" -Location $location `
-	-TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json `
+	-TemplateFile azuredeploy.json `
 	-newStorageAccountName "$($testName)storage" `
 	-virtualNetworkName "$($testName)vnt" `
-	-adVMName "$($testName)advm" `
-	-domainName "$($testName).com" `
-	-addnsName "$($testName)-ad" `
 	-locationFromTemplate $location `
 	-DatabaseServerName "$testName" `
 	-DatabaseName "$($testName)DB" `
 	-TeamCityDNSNameForPublicIP "$($testName)-tcs" `
 	-Verbose
-		
